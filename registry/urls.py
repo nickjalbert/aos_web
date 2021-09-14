62c235fd4316b92945ca8cd33ad3be8d5714c958
Nick Jalbert <nick@jalbert.io>
2021-09-14 13:23:14 +0600

    Create endpoint for AOS CLI to pull registry

--- registry/urls.py
+++ registry/urls.py
@@ -4,6 +4,7 @@ from . import views
 
 urlpatterns = [
     path("", views.index, name="index"),
+    path("api/components/", views.api_components, name="api_components"),
     path(
         "<int:component_id>/", views.component_detail, name="component_detail"
     ),
--- registry/views.py
+++ registry/views.py
@@ -2,6 +2,8 @@ from django.http import HttpResponse
 from django.shortcuts import render  # noqa: F401
 from django.shortcuts import get_object_or_404
 
+import yaml
+
 from .models import Component
 from .models import ComponentRelease
 
@@ -18,3 +20,26 @@ def component_detail(request, component_id):
     component = get_object_or_404(Component, pk=component_id)
     context = {"component": component}
     return render(request, "registry/component_detail.html", context)
+
+
+def api_components(request):
+    all_components = {}
+    for component in Component.objects.all():
+        releases = []
+        component_data = {
+            "type": component.component_type_text.lower(),
+            "description": component.description,
+            "releases": releases,
+        }
+        for release in component.releases.all():
+            release_data = {
+                "name": release.name,
+                "hash": release.git_hash,
+                "github_url": release.github_url,
+                "file_path": release.file_path,
+                "class_name": release.class_name,
+                "requirements_path": release.requirements_path,
+            }
+            releases.append(release_data)
+        all_components[component.name] = component_data
+    return HttpResponse(yaml.dump(all_components))
